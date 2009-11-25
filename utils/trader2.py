728d106e0f4c050bc5258beb69c25b7bf0fd7e45
Manuel Astudillo <manuel@jacaras.com>
2009-11-25 12:37:35 +0100

    Improved the trader simulator.

--- utils/trader2.py
+++ utils/trader2.py
@@ -5,12 +5,18 @@
 
 from retracement import *
 
-MIN_EARNING = 1
-MAX_ENTER_ODDS = 40
-MIN_EXIT_ODDS = 80
+MIN_EARNING = 0
+MAX_ENTER_ODDS = 30
+MIN_EXIT_ODDS = 65
 
-MAX_LOSS = -1.5
-NUM_DAYS = 150
+MAX_LONG_ODDS = 10
+MAX_SHORT_ODDS = 10
+
+ENTER_TRADE_ODDS = 5
+EXIT_TRADE_ODDS = 90
+
+MAX_LOSS = -2
+NUM_DAYS = 100
 
 class Trader(object):
     def __init__(self, cash, courtage):
@@ -48,7 +54,17 @@ class Trader(object):
         
         print "SOLD " + str(amount) + " at " + str(price)         
         self.show_info(price)
-    
+        
+    def buy_all( self, price ):
+        amount = int ( (self.cash - self.courtage) / price)
+        self.buy( amount, price )
+        
+    def sell_all( self, price ):
+        self.sell( trader.stock, price )
+        
+    def short_all( self, price ):
+        amount = int ( (self.cash - self.courtage) / price)
+        self.sell( amount, price )
         
     def earning( self, current_price ):
         if self.stock_price > 0:
@@ -63,7 +79,7 @@ class Trader(object):
             return self.cash - (-self.stock) * current_price
       
     def show_info(self, price):
-        print  "Amount: " + str(self.stock) + " Cash: " + str(self.cash) + "Price: " + str(self.stock_price) + " Value: " + str(self.market_value( price ))
+        print  "Amount: " + str(self.stock) + " Cash: " + str(self.cash) + " Price: " + str(self.stock_price) + " Value: " + str(self.market_value( price ))
           
     
 def accumulateOdds( rl ):
@@ -73,13 +89,29 @@ def accumulateOdds( rl ):
         sum += l[1]
         odds.append( sum )
     return odds
+    
+    
+def findOdds( price, rl ):
+    odds = accumulateOdds( rl )
+        
+    for i in range(0, len(odds)-1):
+        if (price < rl[i][0]) and (price > rl[i+1][0]):
+            d = rl[i][0] - rl[i+1][0]
+            t = ( price - rl[i+1][0] ) / d
+            
+            return t * odds[i] + (1-t)*odds[i+1]
+        elif (price > rl[i][0]) and (price < rl[i+1][0]):
+            d = rl[i+1][0] - rl[i][0]
+            t = ( price - rl[i][0] ) / d
+            
+            return t * odds[i] + (1-t)*odds[i+1]
+
+    return odds[0]
         
 
 def calcEarning( paid_price, sold_price ):
-    #print str(paid_price) + ":" + str(sold_price)
     return ( ( sold_price - paid_price ) / paid_price ) * 100
     
-# risk = 1 - ProbabilityRetracement
 def riskRewardRatio( earning, probability ):
     return  ( probability * earning ) / 100
     
@@ -88,8 +120,7 @@ def enterLong( current_price, trader, btbt, params_btb ):
     t = params_btb[1]
     b2 = params_btb[2]
                 
-    rl = get_retracements( btbt, b1, t, b2 )
-    
+    rl = get_retracements( btbt, b1, t, b2 ) 
     odds = accumulateOdds( rl )
    
     for i in range(0,len(odds)):
@@ -158,81 +189,132 @@ def takeShortProfits( current_price, trader ):
 trader = Trader(100000, 100)
         
 # Simulate trading.
+QUOTE = "../swedbank.txt"
+QUOTE = "../lumi.txt"
+#QUOTE = "../omx30.txt"
+QUOTE = "../volvo b.txt"
 
-input = open("swedbank.txt", "rb")
+input = open(QUOTE, "rb")
 lines = input.readlines()
 input.close()
 
 quotes = []
 
 for index in range(3,len(lines),8):    
-    value = lines[index].strip()
+    value = lines[index].strip().replace(',','')
     if len(value) > 0:
         quotes.append(float(value))
         
 going_up = False
 
-a = compute_tbtb( quotes )
-tbtb = a[1]
+#quotes = quotes[0:NUM_DAYS]
 
-b = compute_btbt( quotes )
-btbt = b[1]
+tbtb = compute_tbtb( quotes )
+btbt = compute_btbt( quotes )
+
 
 # simulate trading
 for i in range(NUM_DAYS,0,-1):   
     new_quotes = quotes[:-i]
 
-    a = compute_tbtb( new_quotes )
-    # tbtb = a[1]
-    params_tbt = a[0] 
-
-    b = compute_btbt( new_quotes )
-    # btbt = b[1]
-    params_btb = b[0]
-      
+    tbt = find_last_t1bt2( new_quotes )
+    btb = find_last_b1tb2( new_quotes )
+    
+    if tbt == None and btb == None:
+        continue
+    
     current_price = new_quotes[-1:][0]
  
-    if params_tbt[2] == current_price:
-        if going_up == False:
-            reversal = True     
-        else:
-            reversal = False
-        going_up = True
-        
-    elif params_btb[2] == current_price:
-        if going_up == True:
-            reversal = True
-        else:
-            reversal = False
-        going_up = False
-               
-    else:
-        print "SOMETHING WRONG!!"
-
-    if trader.stock != 0:
-        if reversal:
-            if trader.stock > 0:
-                if not going_up:
-                    exitLong( current_price, trader, tbtb, params_tbt )
-                else:
-                    trader.sell( trader.stock, current_price )
+    if tbt != None:
+        if tbt[2] == current_price:
+            if going_up == False:
+                reversal = True     
             else:
-                #exitShort( current_price, trader, btbt, params_btb )
-                pass
+                reversal = False
+            going_up = True
+        
+    if btb != None:
+        if btb[2] == current_price:
+            if going_up == True:
+                reversal = True
+            else:
+                reversal = False
+            going_up = False
+ 
+    if trader.stock != 0:
+        if trader.stock > 0:
+            if going_up:
+                
+                rl = get_retracements( btbt, btb[0], btb[1], btb[2] )
+           
+                odds = findOdds( current_price, rl )
+           
+                if odds > MIN_EXIT_ODDS:
+                    trader.sell_all( current_price )
+                
+                 
+                trader.top_price = max( trader.top_price, current_price )
+            else:
+                # Sell if loss > MAX_LOSS from top_price
+                earning = calcEarning( trader.top_price, current_price )
+                if earning < MAX_LOSS:
+                    print "TAKING A LOSS: " + str(earning)
+                    trader.sell_all( current_price )
         else:
-            trader.top_price = max( trader.top_price, current_price )
-            trader.bottom_price = min( trader.bottom_price, current_price )
-           # if trader.stock > 0:
-           #     takeLongProfits( current_price, trader )
-           # else:
-           #     takeShortProfits(current_price, trader )
-  
+            if not going_up:
+                """
+                rl = get_retracements( tbtb, tbt[0], tbt[1], tbt[2] )
+                
+                odds = findOdds( current_price, rl )
+                    
+                if odds > MIN_EXIT_ODDS:
+                    print "EXIT SHORT TRADE"
+                    print "ENTERD LONG TRADE HOPING REVERSAL WITH ODDS: " + str(odds)
+                    trader.buy_all( current_price )
+                    trader.top_price = current_price
+                """
+                trader.bottom_price = min( trader.bottom_price, current_price )
+            else:
+                # Buy if loss > MAX_LOSS from top_price
+                earning = calcEarning( current_price, trader.bottom_price )
+                if earning < MAX_LOSS:
+                    print "TAKING A LOSS: " + str(earning)
+                    trader.buy_all( current_price )
+                    trader.top_price = current_price
+                
     if trader.stock == 0:
         if going_up:
-            enterLong( current_price, trader, btbt, params_btb )
-        else:
-            #enterShort( current_price, trader, tbtb, params_tbt )
-            pass
+            rl = get_retracements( btbt, btb[0], btb[1], btb[2] ) 
+            odds = findOdds( current_price, rl )
+                        
+            if odds < ENTER_TRADE_ODDS:
+                print "ENTER A LONG TRADE WITH ODDS: " + str(odds)
+            
+                trader.buy_all( current_price )
+                trader.top_price = current_price
+            
+            elif odds > EXIT_TRADE_ODDS:
+                print "ENTER A SHORT TRADE HOPING REVERSAL WITH ODDS: " + str(odds)
+                trader.short_all( current_price )
+                trader.bottom_price = current_price
+                 
+        elif tbt != None:
+           
+            rl = get_retracements( tbtb, tbt[0], tbt[1], tbt[2] ) 
+            odds = findOdds( current_price, rl )
+             
+            if odds > EXIT_TRADE_ODDS:
+                print "ENTER A LONG TRADE HOPING REVERSAL WITH ODDS: " + str(odds)
+            
+                trader.buy_all( current_price )
+                trader.top_price = current_price
+             
+            elif odds < ENTER_TRADE_ODDS:
+                print "ENTER A SHORT TRADE WITH ODDS: " + str(odds)
+            
+                trader.short_all( current_price )
+                trader.bottom_price = current_price
+
   
 print "Final Amount: " + str(trader.market_value(current_price))
         
