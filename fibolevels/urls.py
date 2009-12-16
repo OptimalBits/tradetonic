5078b8c8233cbc4dc9b824567b5f620a074b577c
Manuel Astudillo <manuel@jacaras.com>
2009-12-16 12:27:31 +0100

    updated the fibolevels tool, making all of the diagrams dynamic ( uploaded via javascript )

--- fibolevels/urls.py
+++ fibolevels/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns( 'tradetonic.fibolevels.views',
-                        url(r'^quote/(?P<quote_id>[A-Z]+[0-9]+)$', 'update_chart'),
-                        url(r'^params/(?P<quote_id>[A-Z]+[0-9]+)$', 'update_params'),
-                        url(r'^', 'levels') )
+                        url(r'^quote/(?P<quote_id>[A-Z]+[0-9]+)/(?P<resolution>[1-9]+)$', 'update_chart'),
+                        url(r'^levels/(?P<quote_id>[A-Z]+[0-9]+)/(?P<resolution>[1-9]+)$', 'update_levels'),
+                        url(r'^params/(?P<quote_id>[A-Z]+[0-9]+)/(?P<resolution>[1-9]+)$', 'update_params'),
+                        url(r'^menus/quotes/(?P<quote_type>[A-Z]+)$', 'populate_quotes'),
+                        url(r'^', 'entry') )
                         
 
--- fibolevels/views.py
+++ fibolevels/views.py
@@ -12,12 +12,10 @@ import datetime
 class Params:
     pass
 
-quotes = get_market()
-
 resolution = 'Daily'
 histochart = ""
 
-#current_price = quotes[-1:][0]
+shares = get_market()
 
 def embed_svg( svg ):
     return '<script type="image/svg+xml">' + str(svg) + '</script>'
@@ -25,80 +23,106 @@ def embed_svg( svg ):
 def embed_svg2( svg ):
     return '<object data=' + "'data:image/svg+xml," + str(svg) + "'" + 'type="image/svg+xml" width="500" height="500"></object>'
 
-def renderRLChart( quote_id, longReversal = True ):
+def renderRLChart( quote_id, resolution = 1 ):
+    resolution = int(resolution)
+    if resolution >= 1 and resolution < 100:
+        quote_proxy = QuoteProxy( 'quote_cache' )
+        t = quote_proxy.getTicks( quote_id )
     
-    quote_proxy = QuoteProxy( 'quote_cache' )
-    t = quote_proxy.getTicks( quote_id )
+        if resolution != 1:
+            t = t.samplePrices( resolution )
    
-    q = [ float(r) for r in t.getClosePrices() ]
+        q = [ float(r) for r in t.getClosePrices() ]
     
-    params = Params()
+        a = QuoteAnalysis(16, q)
+        
+        going_long = a.isGoingLong()
+        
+        rl = a.get_retracements( *a.last_pattern )
     
-    print q
-    
-    quote_obj = (i for i in quotes if i.id == quote_id ).next()
-
-    if q[-1] <= q[-2]:
-        tbtb = compute_tbtb( q )
-        (t1, b, t2 ) = find_last_t1bt2( q ) 
-        params.a = t1
-        params.b = b
-        params.c = t2
-    
-        rl = get_retracements( tbtb, t1, b, t2 )
-        return ( RetracementLevelsChart("", "Odds of reverting to LONG (%)", quote_obj.longName, rl ), params )
-        #return ( FibonacciChart( "Just a test", "blabla", q, rl ), params )
+        if not going_long:
+            return RetracementLevelsChart("", "Odds of reversing to LONG (%)", "quote_obj.longName", rl )
+        else:            
+            return RetracementLevelsChart("", "Odds of reversing to SHORT (%)","quote_obj.longName", rl )
     else:
-        btbt = compute_btbt( q )
-        ( b1, t, b2 ) = find_last_b1tb2( q )
-        params.a = b1
-        params.b = t
-        params.c = b2
+        return "Invalid Parameters!!"
         
-        rl = get_retracements( btbt, b1, t, b2 )
         
-        return ( RetracementLevelsChart("", "Odds of reverting to SHORT (%)", quote_obj.longName, rl ), params )
+def renderFiboChart( quote_id, resolution = 1 ):
+    
+    resolution = int(resolution)
+    if resolution >= 1 and resolution < 100:
 
+        quote_proxy = QuoteProxy( 'quote_cache' )
+        ticks = quote_proxy.getTicks( quote_id )
+   
+        if resolution != 1:
+            ticks = ticks.samplePrices( resolution )
 
-def update_chart( request, quote_id, longReversal = False ):
-    print quote_id
+        q = [ float(r) for r in ticks.getClosePrices() ]
+        
+        a = QuoteAnalysis(16, q)
+        rl = a.get_retracements( *a.last_pattern )
+        
+        return FibonacciChart( "Fibonacci Levels", "longName", ticks, rl, resolution )
+    return HttpResponse("Invalid parameters: " + str(quote_id) + ":" + str(resolution))
 
-    ( svg, params ) = renderRLChart( quote_id, longReversal )
+def update_chart( request, quote_id, resolution ):
+    svg = renderRLChart( quote_id, int(resolution) )
     return HttpResponse( embed_svg2( svg ) )
     
-def update_params( request, quote_id ):
-    quote_proxy = QuoteProxy( 'quote_cache' )
-    t = quote_proxy.getTicks( quote_id )
-   
-    q = [ float(r) for r in t.getClosePrices() ]
+def update_levels( request, quote_id, resolution ):
+    svg = renderFiboChart( quote_id, int(resolution) )
+    return HttpResponse( embed_svg2( svg ) )
     
-    params = Params()
-    
-    if q[-1] <= q[-2]:
-        (t1, b, t2 ) = find_last_t1bt2( q ) 
-        params.a = t1
-        params.b = b
-        params.c = t2
-        
-        return render_to_response('tbt.html',{'params':params})
+
+# quote_types:
+# 1 - Index
+# 2 - Shares
+def populate_quotes( request, quote_type ):
+    if quote_type == 'INDEXES':
+        quotes = \
+        [ Share('SSESE0000337842', 'SE0000337842', "OMXS30", "OMX Stockholm 30 Index"),
+          Share('CSEDX0000001376', 'DX0000001376', "OMXC20", "OMX Copenhagen 20 Index"),
+          Share('HEXFI0008900212', 'FI0008900212', "OMXH25", "OMX Helsinki 25 Index"),
+          Share('SSESE0001809476', 'SE0001809476', "OMXN40", "OMX Nordic 40 Index"),
+        ]
+
+    elif quote_type == 'SHARES':
+        quotes = shares
     else:
-        ( b1, t, b2 ) = find_last_b1tb2( q )
-        params.a = b1
-        params.b = t
-        params.c = b2
+        quotes = list ()
+
+    return render_to_response('quote_selection.html',{'quotes': quotes})
+
+    
+def update_params( request, quote_id, resolution = 1):
+    resolution = int(resolution)
+    if resolution >= 1 and resolution < 100:
+        quote_proxy = QuoteProxy( 'quote_cache' )
+        t = quote_proxy.getTicks( quote_id )
+    
+        if resolution != 1:
+            t = t.samplePrices( resolution )
+   
+        q = [ float(r) for r in t.getClosePrices() ]
+       
+        params = Params()
+   
+        ( p1, p2, p3, p4 ) = find_last_pattern( q )
+        params.a = p1
+        params.b = p2
+        params.c = p3
+        params.d = q[-1]
         
-        return render_to_response('btb.html',{'params':params})
+        if p1 > p2:
+            return render_to_response('tbt.html',{'params':params})
+        else:
+            return render_to_response('btb.html',{'params':params})
+    return HttpResponse("Invalid parameters: " + str(quote_id) + ":" + str(resolution))
     
-def levels ( request, quote_id = None ):
-    if quote_id == None:
-        quote_id = quotes[0].id
-    
-    ( svg, params ) = renderRLChart( quote_id, False )
-    histochart = embed_svg2( svg )
-    #levelschart = embed_svg2( renderRLChart( quote_id, False ) )
-    levelschart = ""
-    
-    return render_to_response('tradetonic.html', {'quotes': quotes, 'resolution':resolution, 'histochart':histochart, 'levelschart':levelschart, 'params':params } )
+def entry ( request ):    
+    return render_to_response('tradetonic.html')
     
 
     
