99ae7f1dd60516cad3a1b57f7d4fbd071d72e7a0
Manuel Astudillo <manuel@Manuel-Astudillos-MacBook.local>
2010-02-14 11:22:07 +0100

    some changes to fibolevels urlconfs

--- fibolevels/urls.py
+++ fibolevels/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns( 'tradetonic.fibolevels.views',
-                        url(r'^quote/(?P<quote_id>[A-Z]+[0-9]+)/(?P<resolution>[1-9]+)$', 'update_chart'),
-                        url(r'^levels/(?P<quote_id>[A-Z]+[0-9]+)/(?P<resolution>[1-9]+)$', 'update_levels'),
-                        url(r'^params/(?P<quote_id>[A-Z]+[0-9]+)/(?P<resolution>[1-9]+)$', 'update_params'),
+                        url(r'^quote/(?P<quote_id>[A-Z]+[0-9]+)/(?P<resolution>[0-9]+)$', 'update_chart'),
+                        url(r'^levels/(?P<quote_id>[A-Z]+[0-9]+)/(?P<resolution>[0-9]+)$', 'update_levels'),
+                        url(r'^params/(?P<quote_id>[A-Z]+[0-9]+)/(?P<resolution>[0-9]+)$', 'update_params'),
                         url(r'^menus/quotes/(?P<quote_type>[A-Z]+)$', 'populate_quotes'),
-                        url(r'^', 'entry') )
+                        url(r'^charts/', 'show_charts'),
+                        url(r'^bargains/', 'show_bargains'),
+#                        url(r'','show_charts'),
+                      )
                         
 
