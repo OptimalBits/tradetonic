269d761a3388b855156754e40111d92a57eb6725
Manuel Astudillo <manuel@Manuel-Astudillos-MacBook.local>
2010-02-14 11:20:36 +0100

    Many improvements to the retracement odds module.

--- utils/retracement.py
+++ utils/retracement.py
@@ -13,11 +13,12 @@ fibolevels = [ 0.05, 0.0903, 0.1458, 0.236, 0.382, 0.5, 0.618, 0.786, 1, 1.618, 
 # the retracement levels
 #
 class QuoteAnalysis(object):
-    def __init__( self, numFiboLevels, quotes ):
+    def __init__( self, name, numFiboLevels, quotes ):
         self.numfiboLevels = numFiboLevels
         self.quotes = quotes
         
         self.levels = fibolevels
+        self.name = name
         
         self.transform2()
         
@@ -25,7 +26,7 @@ class QuoteAnalysis(object):
         self.tbtb = list()
         self.btbt = list()
     
-        for (p1,p2,p3,p4) in find_pattern( self.quotes ):
+        for (p1,p2,p3,p4,trend) in ( self.quotes ):
             if p4 != None:
                 v1 = (p1 - p2)
                 v2 = (p3 - p2)
@@ -40,14 +41,15 @@ class QuoteAnalysis(object):
                     self.tbtb.append((theta, fibolevel))
                 else:
                     self.btbt.append((theta, fibolevel))
-        self.last_pattern = ( p1, p2, p3, p4 )
+        self.last_pattern = ( p1, p2, p3, p4, trend )
         
     def transform2( self ):             
         self.tbtb = list()
         self.btbt = list()
         self.data = list()
     
-        for (p1,p2,p3,p4) in find_pattern( self.quotes ):
+        ( p1, p2, p3, p4, trend ) = ( None, None, None, None, None )
+        for (p1,p2,p3,p4,trend) in find_pattern( self.quotes ):
             if p4 != None:
                 v1 = (p1 - p2)
                 v2 = (p3 - p2)
@@ -65,7 +67,7 @@ class QuoteAnalysis(object):
                     self.tbtb.append((in_level, out_level))
                 else:
                     self.btbt.append((in_level, out_level))
-        self.last_pattern = ( p1, p2, p3, p4 )
+        self.last_pattern = ( p1, p2, p3, p4, trend )
         
     def get_histogram2(self, p1, p2, p3):
         histogram = [0 for i in range(self.numfiboLevels)]
@@ -124,7 +126,9 @@ class QuoteAnalysis(object):
 
         return histogram
         
-    def get_retracements( self, p1, p2, p3, p4 = None ):
+    def get_retracements( self, p1, p2, p3, p4 = None, trend = None ):
+        if p1 == None or p2 == None or p3 == None:
+            return []
         histogram = self.get_histogram2( p1, p2, p3)
         
         v = ( p3 - p2 )
@@ -132,6 +136,33 @@ class QuoteAnalysis(object):
         rl = [ (p3 - v * self.levels[i], 100 * histogram[i]) for i in range(len(self.levels)) ]
     
         return rl
+        
+    def get_trend( self ):
+        return "{0:.2f}%".format(100 * self.last_pattern[4])
+        
+    def get_current_odds( self, p1 = None, p2 = None, p3 = None, p4 = None, trend = None ):
+        if p1 == None or p2 == None or p3 == None or p4 == None:
+            ( p1, p2, p3, p4, trend ) =  self.last_pattern
+    
+        rls = self.get_retracements( p1, p2, p3, p4 )
+        
+        if p4 != None:
+            last_price = p4
+        else:
+        	if len(self.quotes) >= 1:
+	            last_price = self.quotes[-1]
+	        else:
+	        	return 0
+        
+        odds = []
+        sum = 0
+        for l in rls:
+            if (p2 >= p3 and last_price > l[0]) or (p2 <= p3 and last_price < l[0]):
+                sum += l[1]
+                odds.append( sum )    
+        
+        return sum
+            
         
     def isGoingLong( self ):
         return self.last_pattern[2] < self.last_pattern[3]
@@ -172,6 +203,8 @@ def find_pattern( quotes ):
     state = 0
     i = 0
     
+    filtered_quotes = lowPassFilter(quotes, 15)
+    
     while i < len(quotes):
         val = quotes[i]
         
@@ -198,8 +231,13 @@ def find_pattern( quotes ):
                 p3 = val
                 
         elif state == 4:
-            if (p3 < p4 and val < p4) or (p3 > p4 and val > p4):
-                yield ( p1, p2, p3, p4 )
+            if (p3 < p4 and val < p4) or (p3 > p4 and val > p4):                
+                
+                d = filtered_quotes[i] - filtered_quotes[i-1]
+                theta = math.atan2( d, 1 ) # angle in radians.
+                trend = theta / (math.pi/2)
+                
+                yield ( p1, p2, p3, p4, trend )
                    
                 p1 = p2
                 p2 = p3
@@ -208,22 +246,73 @@ def find_pattern( quotes ):
         
         i = i + 1
 
+	if len(filtered_quotes) >= 2:
+	    d = filtered_quotes[-1] - filtered_quotes[-2]
+    	theta = math.atan2( d, 1 ) # angle in radians.
+    	trend = theta / (math.pi/2)
+    else:
+    	trend = 0
+                
     if state >= 3:
-        yield ( p1, p2, p3, val )
+        yield ( p1, p2, p3, val, trend )
 
 def find_last_pattern( quotes ):
     found = False
     
-    for ( p1, p2, p3, p4 ) in find_pattern( quotes ):
+    for ( p1, p2, p3, p4, trend ) in find_pattern( quotes ):
         found = True
         pass
     
     if found:
-        return ( p1, p2, p3, p4 )
+        return ( p1, p2, p3, p4, trend )
     else:
         return None
     
+    
+def rectWeights( supportSize ):
+    return [1/float(supportSize)] * supportSize
+    
+def gaussian( tau, sigma, supportSize ):
+    pass
+    
+def lowPassFilter( data, supportSize ):
+    
+   # weights = weightFunc( supportSize )
+    
+    halfSupport = supportSize/2
 
+    out_data = []
+
+    for i in range(len(data)):
+        start = max(0, i - halfSupport)
+        end = i + halfSupport + 1
+        kernel = data[start:end]
+        
+        d = float(len(kernel))
+        if d  > 0:
+            s = 1 / d
+            out_data.append( sum( x * s for x in kernel ) )
+    return out_data
+            
+def get_bargains( analysis_list ):
+    bargains = []
+    for a in analysis_list:
+        odds = a.get_current_odds( *a.last_pattern )
+        trend = a.last_pattern[4]
+        if odds > 80:
+            if (trend > 0.05) and (a.last_pattern[0] > a.last_pattern[1]):
+                bargains.append( (a, 'Buy') )
+                print trend
+            elif (trend < -0.05) and (a.last_pattern[0] < a.last_pattern[1]):
+                bargains.append( (a, 'Sell') )
+                print trend
+                
+    return bargains
+    
+ 
+    
+    
+    
     
     
     
