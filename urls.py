e3978b9a2ade152b899cdc2bfa58b412f7bc2d0a
Manuel Astudillo <manuel@Manuel-Astudillos-MacBook.local>
2010-02-14 11:24:56 +0100

    changes to main urlconfs.

--- urls.py
+++ urls.py
@@ -4,9 +4,29 @@ from django.conf.urls.defaults import *
 # from django.contrib import admin
 # admin.autodiscover()
 
+from tradetonic.tabmenu.models import *
 
-urlpatterns = patterns( '',
+
+tabs = ( Tab("Home", "", 'tradetonic.views.home'),
+         Tab("Fibonacci Levels", "fibonacci", include('tradetonic.fibolevels.urls'),
+            ( Tab("Chart","charts", ""), Tab("Money Management","money", ""), Tab("Bargains","bargains", "") ) ), 
+         Tab("Tutorials", "tutorials", "tradetonic.views.tutorials"), 
+         Tab("News & Press", "news", "tradetonic.views.news"),
+         Tab("Disclaimer", "news", "tradetonic.views.news"),
+         Tab("Contact Us", "contact", "tradetonic.views.contact") )
+
+
+urlpatterns = patterns('',
     # Static Media
-   (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': '/Users/manuel/Django-projects/tradetonic/media'}),
-   (r'^', include ('tradetonic.fibolevels.urls')),
+   (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': '/Users/manuel/dev/Django-projects/tradetonic/media'})
 )
+
+for t in tabs:
+	urlpatterns += patterns('', t.url_pattern(tabs) )
+	print t.url_pattern(tabs)
+    
+    
+    
+
+
+
