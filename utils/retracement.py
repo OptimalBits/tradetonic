326810b1e4963d9b37c6782651b2afd23c3e6915
Manuel Astudillo <manuel@Manuel-Astudillos-MacBook.local>
2010-03-20 19:47:21 +0100

    Added a new analysis function.

--- utils/retracement.py
+++ utils/retracement.py
@@ -4,9 +4,85 @@ import math
 #
 #
 
-NUM_FIBO_LEVELS = 16
-
 fibolevels = [ 0.05, 0.0903, 0.1458, 0.236, 0.382, 0.5, 0.618, 0.786, 1, 1.618, 2.618, 4.235, 6.857, 11.077, 18, 28 ]
+
+
+#
+# QuoteAnalysisEx
+#
+# Computes the statistics for V formed patterns, where 
+# the number of days is associated to the retracement to some fibonacci level.
+#
+#
+class QuoteAnalysisEx(object):
+    def __init__( self, name, fibolevels, quotes ):
+        self.levels = fibolevels
+        self.quotes = quotes
+        self.name = name
+        
+        self.transform()
+        
+    # transform data to ( value_level, days_up/down ) tuplets
+    def transform():
+        self.tbt = list()
+        self.btb = list()
+       
+        ( p1, nd12, p2, nd23, p3 ) = ( None, None, None, None, None )
+        for (p1, nd12, p2, nd23, p3) in find_V_pattern( self.quotes ):
+            if p4 != None:
+                v1 = (p1 - p2)
+                v2 = (p3 - p2)
+
+                val_ratio = v2 / v1
+                time_ratio = nd23 / nd12
+               
+                val_level = quantize_level(self.levels, val_ratio)
+                time_level = quantize_level(self.levels, time_ratio)
+               
+                self.data.append((time_level, val_level))
+                if p1 > p2:
+                    self.tbt.append((time_level, val_level))
+                else:
+                    self.btb.append((in_level, out_level))
+        self.last_pattern = (p1, nd12, p2, nd23, p3)
+        
+    def get_histogram(self, p1, p2, p3):
+        histogram = [0 for i in range(len(self.levels))]
+
+        v1 = (p1 - p2)
+        v2 = (p3 - p2)
+        val_ratio = v2 / v1
+        val_level = quantize_level(self.levels, val_ratio)
+
+        if p1 > p2:
+            data = self.tbtb
+        else:
+            data = self.btbt
+
+        for l in data:
+            if l[0] == val_level and l[1] != None:
+                histogram[l[1]] += 1
+
+        s = float(sum(histogram))
+
+        if s > 0:
+            for i in range(len(self.levels)):
+                histogram[i] /= s
+
+        return histogram
+        
+    def get_retracements( self, p1, p2, p3 = None ):
+        if p1 == None or p2 == None:
+            return []
+        histogram = self.get_histogram( p1, p2, p3)
+        
+        v = ( p3 - p2 )
+        
+        rl = [ (p3 - v * self.levels[i], 100 * histogram[i]) for i in range(len(self.levels)) ]
+    
+        return rl
+        
+        
 
 #
 # Represents the analysis of a quote that can be used to retrieve 
@@ -49,7 +125,7 @@ class QuoteAnalysis(object):
         self.data = list()
     
         ( p1, p2, p3, p4, trend ) = ( None, None, None, None, None )
-        for (p1,p2,p3,p4,trend) in find_pattern( self.quotes ):
+        for (p1,p2,p3,p4,trend) in find_N_pattern( self.quotes ):
             if p4 != None:
                 v1 = (p1 - p2)
                 v2 = (p3 - p2)
@@ -149,10 +225,10 @@ class QuoteAnalysis(object):
         if p4 != None:
             last_price = p4
         else:
-        	if len(self.quotes) >= 1:
-	            last_price = self.quotes[-1]
-	        else:
-	        	return 0
+            if len(self.quotes) >= 1:
+                last_price = self.quotes[-1]
+            else:
+                return 0
         
         odds = []
         sum = 0
@@ -199,7 +275,7 @@ def quantize_level(levels, r):
 #
 #
     
-def find_pattern( quotes ):    
+def find_N_pattern( quotes ):    
     state = 0
     i = 0
     
@@ -246,12 +322,12 @@ def find_pattern( quotes ):
         
         i = i + 1
 
-	if len(filtered_quotes) >= 2:
-	    d = filtered_quotes[-1] - filtered_quotes[-2]
-    	theta = math.atan2( d, 1 ) # angle in radians.
-    	trend = theta / (math.pi/2)
+    if len(filtered_quotes) >= 2:
+        d = filtered_quotes[-1] - filtered_quotes[-2]
+        theta = math.atan2( d, 1 ) # angle in radians.
+        trend = theta / (math.pi/2)
     else:
-    	trend = 0
+        trend = 0
                 
     if state >= 3:
         yield ( p1, p2, p3, val, trend )
@@ -259,7 +335,7 @@ def find_pattern( quotes ):
 def find_last_pattern( quotes ):
     found = False
     
-    for ( p1, p2, p3, p4, trend ) in find_pattern( quotes ):
+    for ( p1, p2, p3, p4, trend ) in find_N_pattern( quotes ):
         found = True
         pass
     
@@ -267,6 +343,43 @@ def find_last_pattern( quotes ):
         return ( p1, p2, p3, p4, trend )
     else:
         return None
+        
+        
+def find_V_pattern( quotes ):    
+    state = 0
+    i = 0
+       
+    while i < len(quotes):
+        val = quotes[i]
+        
+        if state == 0:
+            p1 = val
+            num_days = 0
+            state = 1
+            
+        elif state == 1:
+            p2 = val
+            state = 2
+                
+        elif state == 2:
+            if ( p1 < p2 and val < p2 ) or ( p1 > p2 and val > p2 ): 
+                p3 = val
+                num_days_prev = num_days
+                state = 3
+            else:
+                p2 = val
+                
+        elif state == 3:
+            if ( p2 < p3 and val < p3 ) or ( p2 > p3 and val > p3 ):
+                
+                yield( p1, num_days_prev, p2, num_days, p3 )
+            p3 = val
+        
+        i = i + 1
+        num_days = num_days + 1
+                
+    if state >= 2:
+        yield( p1, num_days_prev, p2, num_days, val )
     
     
 def rectWeights( supportSize ):
